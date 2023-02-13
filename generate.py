466849955042bdc51f2615cfd6134f6e6d9d4b64
shirleyzzr1 <zhuoruzhang2021@u.northwestern.edu>
2023-02-13 17:05:20 -0600

    Add many different links

--- generate.py
+++ generate.py
@@ -1,5 +1,16 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box",pos=[0,0,0.5],size=[1,1,1])
+pyrosim.Start_SDF("boxes.sdf")
+length,width,height = 1,1,1
+x,y,z = 0,0,0.5
+for i in range(10):
+    pyrosim.Send_Cube(name="Box",pos=[x,y,z],size=[length,width,height])
+    z+=(height/2)
+    length*=0.9
+    width*=0.9
+    height*=0.9
+    z+=(height/2)
+
+#pyrosim.Send_Cube(name="Box",pos=[x,y,z],size=[length,width,height])
+#pyrosim.Send_Cube(name="Box2",pos=[x2,y2,z2],size=[length,width,height])
 pyrosim.End()
 
--- simulate.py
+++ simulate.py
@@ -1,8 +1,12 @@
 import pybullet as p
 import time
+import pybullet_data
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 #p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
-p.loadSDF("box.sdf")
+p.setGravity(0,0,-9.8)
+p.loadURDF("plane.urdf")
+p.loadSDF("boxes.sdf")
 for i in range(1000):
     p.stepSimulation()
     print("i: ",i)
