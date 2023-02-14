8afb1b5ba9b15b7b9f3e93d17adc4bec2a9e6280
shirleyzzr1 <zhuoruzhang2021@u.northwestern.edu>
2023-02-13 22:09:51 -0600

    generate a 3link 2joint robot

--- generate.py
+++ generate.py
@@ -10,21 +10,11 @@ def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
     length,width,height = 1,1,1
     x,y,z = 0,0,0.5
-    pyrosim.Send_Cube(name="Link0",pos=[0,0,0.5],size=[length,width,height])
-    pyrosim.Send_Joint(name="Link0_Link1",parent = "Link0",child = "Link1",type="revolute",position = [0,0,1.0])
-    pyrosim.Send_Cube(name="Link1",pos=[0,0,0.5],size=[length,width,height])
-    pyrosim.Send_Joint(name="Link1_Link2",parent = "Link1",child = "Link2",type ="revolute",position = [0,0,1.0])
-    pyrosim.Send_Cube(name="Link2",pos=[0,0,0.5],size=[length,width,height])
-    pyrosim.Send_Joint(name="Link2_Link3",parent = "Link2",child = "Link3",type ="revolute",position = [0,0.5,0.5])
-    pyrosim.Send_Cube(name="Link3",pos=[0,0.5,0],size=[length,width,height])
-    pyrosim.Send_Joint(name="Link3_Link4",parent = "Link3",child = "Link4",type ="revolute",position = [0,1.0,0])
-    pyrosim.Send_Cube(name="Link4",pos=[0,0.5,0],size=[length,width,height])
-    pyrosim.Send_Joint(name="Link4_Link5",parent = "Link4",child = "Link5",type ="revolute",position = [0,0.5,-0.5])
-    pyrosim.Send_Cube(name="Link5",pos=[0,0,-0.5],size=[length,width,height])
-    pyrosim.Send_Joint(name="Link5_Link6",parent = "Link5",child = "Link6",type ="revolute",position = [0,0,-1.0])
-    pyrosim.Send_Cube(name="Link6",pos=[0,0,-0.5],size=[length,width,height])
-
-
+    pyrosim.Send_Cube(name="Torso",pos=[0,0,0.5],size=[length,width,height])
+    pyrosim.Send_Joint(name="Torso_BackLeg",parent = "Torso",child = "BackLeg",type="revolute",position = [0,0.5,1.0])
+    pyrosim.Send_Cube(name="BackLeg",pos=[0,0.5,0.5],size=[length,width,height])
+    pyrosim.Send_Joint(name="BackLeg_FrontLeg",parent = "BackLeg",child = "FrontLeg",type ="revolute",position = [0,1.0,0])
+    pyrosim.Send_Cube(name="FrontLeg",pos=[0,0.5,-0.5],size=[length,width,height])
     pyrosim.End()
 
 Create_World()
