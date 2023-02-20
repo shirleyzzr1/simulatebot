d1bd79d33f38d52841b1679b0fba14588a85b46d
shirleyzzr1 <zhuoruzhang2021@u.northwestern.edu>
2023-02-20 10:54:15 -0600

    Change robot to a quadruped

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 import numpy as np
 STEP = 1000
 maxForce = 20
-SLEEP = 1.0/1000
+SLEEP = 1.0/100
 Gravity = -9.8
 Bamplitude = np.pi/4
 Bfrequency = 10
@@ -10,4 +10,7 @@ Famplitude = np.pi/4
 Ffrequency = 10
 FphaseOffset = 0
 numberOfGenerations = 10
-populationSize = 2
+populationSize = 10
+numSensorNeurons = 9
+numMotorNeurons = 8
+motorJointRange= 0.2
